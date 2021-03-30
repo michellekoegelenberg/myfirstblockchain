e8f90a8e87fd1dc905e26f79b9b21dbb99feaf32
michellekoegelenberg <38162974+michellekoegelenberg@users.noreply.github.com>
2021-03-30 11:49:59 +0200

    My first blockchain
    :)

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/michellekoegelenberg/myfirstblockchain
+
+go 1.15
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/michellekoegelenberg/myfirstblockchain/blockchain"
+)
+
+func main() {
+	chain := blockchain.InitBlockChain()
+	chain.AddBlock("First block after Genenis")
+	chain.AddBlock("Second block after Genesis")
+	chain.AddBlock("Third block after Genesis")
+	for _, block := range chain.Blocks {
+		//fmt.Printf("Previous Hash: %x\n", block.PrevHash)
+		fmt.Printf("Data in Block: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+	}
+}
