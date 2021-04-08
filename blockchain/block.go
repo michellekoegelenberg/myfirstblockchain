ea43432fdcdfdc5a5b889390468cf3fe342d9110
michellekoegelenberg <38162974+michellekoegelenberg@users.noreply.github.com>
2021-04-08 13:06:37 +0200

    Adding new test file

--- blockchain/block.go
+++ blockchain/block.go
@@ -3,6 +3,8 @@ package blockchain
 type BlockChain struct {
 	Blocks []*Block
 }
+
+//Modify Block to store nonce so validation func can be implemented
 type Block struct {
 	Hash     []byte
 	Data     []byte
@@ -10,9 +12,15 @@ type Block struct {
 	Nonce    int
 }
 
+//Add 0 for initial nonce (after prevHash)
+//Modify CreateBlock so that it runs the pow algo on each block we create
+//Execute the run func on that pow which will return the nonce and hash
+//Put the nonce and hash into the block structure
+//Return the block
+//Now we can pass around data properly :)
 func CreateBlock(data string, prevHash []byte) *Block {
-	block := &Block{[]byte{}, []byte(data), prevHash, 0}
-	pow := NewProof(block)
+	block := &Block{[]byte{}, []byte(data), prevHash, 0} //Block gets initjsvdjksvbd
+	pow := NewProof(block)                               //Init pow with NewProof allows us to pair a target with each new block that gets created
 	nonce, hash := pow.Run()
 
 	block.Hash = hash[:]
