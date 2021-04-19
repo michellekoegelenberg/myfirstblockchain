0c36c6c4d33c7b559ff3a89892b05d8894567089
michellekoegelenberg <38162974+michellekoegelenberg@users.noreply.github.com>
2021-04-19 13:48:47 +0200

    Part 1 of chapter 3 complete

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,6 +1,8 @@
 package blockchain
 
 import (
+	"fmt"
+
 	"github.com/dgraph-io/badger/v3"
 )
 
@@ -17,9 +19,76 @@ func InitBlockChain() *BlockChain {
 	opts := badger.DefaultOptions //Options struct
 	opts.Dir = dbPath             //Where database will store keys
 	opts.ValueDir = dbPath        //Where database will store values
+
+	 //Could try this:   db, err := badger.Open(badger.DefaultOptions("/tmp/badger"))
+
+	db, err := badger.Open(opts)
+
+	Handle(err)
+
+	//Because we are initialising the blockchain, we need write capabilities: Hence, Update func
+	err = db.Update(func(txn *badger.Txn) error {
+		//Inside closure we have access to the transaction
+		if _, err := txn.Get([]byte("lh")); err == badger.ErrKeyNotFound { // Check if there is blockchain in db
+			fmt.Println("No existing blockchain found")
+			genesis := Genesis()                              //Creating genesis block
+			fmt.Println("Genesis proved")                     //Proving it
+			err := txn.Set(genesis.Hash, genesis.Serialize()) //Genesis hash is key for Genesis block, Serialize gen block
+			//Put it into database using transaction set func
+			Handle(err)
+			//Gen block hash is the only hash in the database, so set it to the key of lh
+			err = txn.Set([]byte("lh"), genesis.Hash)
+
+			lastHash = genesis.Hash //Save to var so we can put it into memory storage
+
+			return (err)
+		} else { //If we already have a db
+			//Get the lh from the db
+			item, err := txn.Get([]byte("lh")) //Call get on the key (lh). This gives pointer to item struct and err
+			Handle(err)
+			lastHash, err = item.ValueCopy(nil) //Changed this in accordance with new BadgerDB API
+   //Get value from item struct & put into lastHash var
+			return err                   //Return 1st err and handke 2nd err above
+		}
+	})
+
+	Handle(err)
+
+	blockchain := BlockChain{lastHash, db} //Create new blockchain in memory
+	return &blockchain                     //This way we can use it further in our app
 }
 
 func (chain *BlockChain) AddBlock(data string) {
+	var lastHash []byte
+	//Execute a read-only type of txn on our BadgerDB (call the database from our blockchain by calling on chain.Database)
+	//Call View (read-only), which takes in a closure with a pointer to a Badger transaction
+	//Returns an err
+   err := chain.Database.View(func(txn *badger.Txn)) error {
+	//Get the current last hash out of the database (the hash of the last block in our database)
+	//Call txn.Get to get the item, then unwrap the value from item and put it into our lastHash var
+	//Return error if there is one
+	//Handle the first err if there is one
+	item, err := txn.Get(byte[]("lh"))
+	Handle(err)
+	lastHash, err = item.Value()
+	return err
+   }
+	Handle(err)
+	//Create a new block with the lastHash and the data we are passing in to the AddBlock func
+   newBlock := CreateBlock(data, lastHash)
+   //With our new block now created, we want to do a read/write type txn on our db
+   //so we can put the new block into the database and assign the new block's hash to our last hash key
+   err := chain.Database.Update(func(txn *badger.Txn) error) {
+	   err := txn.Set(newBlock.Hash, newBlock.Serialize())
+	   Handle(err)
+
+   }
+	
+	
+
+
+
+
 	prevBlock := chain.Blocks[len(chain.Blocks)-1]
 	new := CreateBlock(data, prevBlock.Hash)
 	chain.Blocks = append(chain.Blocks, new)
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/michellekoegelenberg/myfirstblockchain
 
 go 1.15
 
-require github.com/dgraph-io/badger/v3 v3.2011.1 // indirect
+require (
+	github.com/dgraph-io/badger v1.6.2 // indirect
+	github.com/dgraph-io/badger/v3 v3.2011.1 // indirect
+)
